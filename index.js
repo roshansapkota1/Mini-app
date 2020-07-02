869701a5f8d1271e0654679f76d900d78b7de2fc
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-07-02 22:22:40 +0200

    evaluator app and form validation revision

--- index.js
+++ index.js
@@ -1,66 +1,110 @@
+const form = document.querySelector('#form')
+const container = document.querySelector('.container')
 
-var root = document.querySelector('.root')
+const htmlInput = document.querySelector('#htmlScore')
+const cssInput = document.querySelector('#cssScore')
+const jsInput = document.querySelector('#jsScore')
+const backendInput = document.querySelector('#backendScore')
+let renderAverage = document.createElement('h3')
+renderAverage.className = 'renderAverage'
 
-const html = document.createElement('input')
-const css = document.createElement('input')
-const javascript = document.createElement('input')
-const react = document.createElement('input')
-const backend = document.createElement('input')
-var button = document.createElement('button')
-var message = document.createElement('h3')
-button.innerText = 'button'
+let inputarray = [htmlInput, cssInput, jsInput, backendInput]
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    cheeckInputs()
+})
 
-html.type = 'text'
-html.placeholder = 'name'
-
-
-
-
-
-const getVAlue = () =>{
-let htmlValue = parseFloat(html.value);
-let cssValue = parseFloat(css.value);
-let javaScriptValue = parseFloat(javascript.value);
-let reactValue = parseFloat(react.value);
-let backendValue = parseFloat(backend.value);
-
-let average = (htmlValue + cssValue + javaScriptValue + reactValue + backendValue) / 5;
-if (average > 0){root.appendChild(message)}
-
-console.log(average);
-
-
-
-if (average > 9){
-    message.innerText = 'congratulations ! excellent A grade'
-    
-}
-
-
-
-
-
-
-
-
+const cheeckInputs = () => {
+    renderAverage.innerText = ''
+   const htmlValue =  htmlInput.value
+    const cssValue = cssInput.value
+    const jsValue = jsInput.value
+    const backendValue = backendInput.value
  
+    
+
+    if ( htmlValue === ''){
+        //show error
+        //add error class
+        setErrorFor(htmlInput, 'This field cannot be left blank')
+    }else{
+        //add success class
+       // setSuccessFor(htmlInput,'This field cannot be left blank')
+       setSuccessFor(htmlInput)
+    }
+
+    if ( cssValue === ''){
+        //show error
+        //add error class
+        setErrorFor(cssInput, 'This field cannot be left blank')
+    }else{
+        //add success class
+       // setSuccessFor(htmlInput,'This field cannot be left blank')
+       setSuccessFor(cssInput)
+    }
+    if ( jsValue === ''){
+        //show error
+        //add error class
+        setErrorFor(jsInput, 'This field cannot be left blank')
+    }else{
+        //add success class
+       // setSuccessFor(htmlInput,'This field cannot be left blank')
+       setSuccessFor(jsInput)
+    }
+    if ( backendValue === ''){
+        //show error
+        //add error class
+        setErrorFor(backendInput, 'This field cannot be left blank')
+    }else{
+        //add success class
+       // setSuccessFor(htmlInput,'This field cannot be left blank')
+       setSuccessFor(backendInput)
+    }
+    if ( htmlValue !== '' && cssValue !== '' && jsValue !== '' && backendValue !== ''){
+        
+        const valueArray = [htmlValue, cssValue, jsValue, backendValue]
+        const valueArrayAfterParse = valueArray.map(any => parseFloat(any))
+        let total = 0
+        for (let i = 0; i < valueArrayAfterParse.length; i++) {
+            total += valueArrayAfterParse[i]
+        } 
+        let average = total /  valueArrayAfterParse.length
+        console.log(total);
+        
+        
+        renderAverage.innerText = `Your average is ${average}`
+        container.appendChild(renderAverage);
+
+        htmlInput.value = ''
+        cssInput.value = ''
+        jsInput.value = ''
+        backendInput.value = ''
+
+        inputarray.forEach(any => any.parentElement.className = 'form-control')
+
+        
+    }
+}
+
+const setErrorFor = (input, message) => {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    //adding error message inside small 
+    small.innerText = message;
+    //adding error class 
+    formControl.className = 'form-control error'
+    
 
 }
 
-
-
-
-button.addEventListener('click', getVAlue)
-
-
-root.appendChild(html);
-root.appendChild(css);
-root.appendChild(javascript);
-root.appendChild(react);
-root.appendChild(backend);
-root.appendChild(button);
-
+const setSuccessFor = (input) => {
+    const formControl = input.parentElement
+    formControl.className = 'form-control success'
+    
+   
+}
 
 
 
